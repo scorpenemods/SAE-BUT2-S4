64d21a13dd1592a1fd5197356c82893f7ab11acb
Remy <remy.boulet@uphf.fr>
2024-09-17 11:05:57 +0200

    formulaire création de compte et création de la page valider

---
+++ Model/AccountCreation/AccountCreation.php
@@ -0,0 +1,54 @@
+<?php
+
+include 'Service/DB.php';
+session_start();
+
+
+#Si le formulaire est rempli correctement on redirige vers une page de validation
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header('location: AccountValidAccountValidation.php');
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Création du compte</h1>
+<form action="AccountCreation.php" method="post">
+    <p>
+        <input type="radio" name="choice" value="student" id="student" required />
+        <label for="student">Étudiant</label>
+
+        <input type="radio" name="choix" value="tutorprofessor" id="tutorprofessor" required />
+        <label for="tutorprofessor">Professeur referant :</label>
+
+        <input type="radio" name="choix" value="tutorcompany" id="tutorcompany" required />
+        <label for="tutorcompany">Tuteur professionnel :</label>
+    </p>
+    <p>
+        <label for="function">Activité professionnelle/universitaire :</label>
+        <input name="function" id="function" type="text" required/>
+    </p>
+    <p>
+        <label for="email">E-mail :</label>
+        <input name="email" id="email" type="text" required/>
+    </p>
+    <p>
+        <label for="name">Nom :</label>
+        <input name="name" id="name" type="text" required/>
+    </p>
+    <p>
+        <label for="firstname">Prénom :</label>
+        <input name="firstname" id="firstname" type="text" required/>
+    </p>
+
+    <p>
+        <label for="phone">Téléphone :</label>
+        <input name="phone" id="phone" type="text" required/>
+    </p>
+
+    <button type="submit">Valider</button>
+</form>
+
+</body>
+</html>
--- Service/DB.php
+++ Service/DB.php
@@ -1,1 +1,4 @@
 <?php
+
+$pdo = new PDO("mysql:host=localhost;dbname=td7et8", 'root', 'root');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
