c6c3b34789f05692e014f46aec7d26f2d8c2b63e
Remy <remy.boulet@uphf.fr>
2024-09-17 11:34:22 +0200

    Information de validation et redirection

---
+++ Model/Validation/Validation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Demande envoyée</h1>
+<p>Retour à la page d'accueil dans 3 secondes...</p>
+
+<script>
+    // JavaScript redirige vers l'index après 3 seconds
+    setTimeout(function() {
+        window.location.href = "Index/index.php";
+    }, 3000);
+</script>
+
+</body>
+</html>
