a6cf9207bdefe1d18a679416a4981a5c1c37e857
noahlemr <noahlemr@gmail.com>
2024-09-18 15:05:29 +0200

    CSS pour page de redirection

--- Model/Redirection/Redirection.php
+++ Model/Redirection/Redirection.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirection</title>
+    <link rel="stylesheet" href="Redirection.css"> <!-- Lien vers le fichier CSS -->
+</head>
 <body>
-<h1>Redirection...</h1>
-<p>Retour à la page d'accueil dans 3 secondes...</p>
+<div class="container">
+    <h1>Redirection...</h1>
+    <p>Retour à la page d'accueil dans 3 secondes...</p>
+</div>
 
 <script>
     // JavaScript redirige vers l'index après 3 seconds
@@ -10,6 +18,5 @@
         window.location.href = "Index/index.php";
     }, 3000);
 </script>
-
 </body>
 </html>
