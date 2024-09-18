86535ad549f27811b4c67070c34e589bec532414
jubrg <julien.bourgeois4@icloud.com>
2024-09-18 16:06:57 +0200

    changement du header par celui de valérie

--- Model/Principal/Principal.js
+++ Model/Principal/Principal.js
@@ -21,6 +21,30 @@ function turn() {
     }
 }
 
+function toggleLanguage() {
+    const languageSwitch = document.getElementById('language-switch');
+    if (languageSwitch.checked) {
+        console.log('Switch to English');
+    } else {
+        console.log('Switch to French');
+    }
+}
+
+
+function toggleTheme() {
+    const themeSwitch = document.getElementById('theme-switch');
+    if (themeSwitch.checked) {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+        console.log('Dark theme enabled');
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+        console.log('Light theme enabled');
+    }
+}
+
+
 // Code menus principaux
 function widget(x) {
     // Récupère la ligne ayant la classe "Visible" pour la supprimer et la remplacer par la classe "Contenu"
