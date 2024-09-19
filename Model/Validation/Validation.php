63c42aee58c509f13e5ca5d3fcd9e5e901a55680
Marion <Marion.Hethuin@uphf.fr>
2024-09-19 11:45:39 +0200

    Page validation à compléter avec base de donnée

---
+++ Model/Validation/Validation.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/Model/DefaultStyles/styles.css ">
+</head>
+<body>
+<h3 class="main-content"> En attente de validation... </h3>
+</body>
+</html>
