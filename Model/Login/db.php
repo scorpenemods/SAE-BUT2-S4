8976cf3f58fd82faca43e387ca87d8b9a4b035cb
noahlemr <noahlemr@gmail.com>
2024-09-19 10:14:49 +0200

    fonction bdd good

--- Model/Login/db.php
+++ Model/Login/db.php
@@ -1,17 +1,17 @@
 <?php
-$host = 'localhost';
-$port = '3306';
-$db = 'saeDB';
-$user = 'root';
-$pass = 'loptro342004';
-
-try {
-    $pdo = new PDO($host,$host, $user, $pass);
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
+//$host = 'localhost';
+//$port = '3306';
+//$db = 'saeDB';
+//$user = 'root';
+//$pass = 'loptro342004';
+//
+//try {
+//    $pdo = new PDO($host,$host, $user, $pass);
+//    // Set the PDO error mode to exception
+//    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//} catch (PDOException $e) {
+//    die("Connection failed: " . $e->getMessage());
+//}
 
 #$password = 'sae';
 #$password_hash = password_hash($password, PASSWORD_BCRYPT);
