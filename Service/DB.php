2cd55c0cfe47ec570b41161f7f7c70e715511f3c
Remy <remy.boulet@uphf.fr>
2024-09-19 10:57:12 +0200

    connexion a la db

--- Service/DB.php
+++ Service/DB.php
@@ -1,3 +1,6 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=td7et8", 'root', 'root');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+include "../Class/DataBase.php";
+
+$db = new Database("141.94.245.139", "s3081_BDD_Barkhane", "u3081_erRWAWL7zt", "ODyKebC@rSeyavay2Olz4!K!");
+
