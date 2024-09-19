fb96139ca2597ab501a34ad22e4a3dd2596c3a0f
Marion <Marion.Hethuin@uphf.fr>
2024-09-19 10:18:20 +0200

    Page déconnexion

--- Model/Deconnexion/Deconnexion.php
+++ Model/Deconnexion/Deconnexion.php
@@ -1,7 +1,18 @@
 <?php
-
 session_start();
 
+$_SESSION = array();
+
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+session_destroy();
 ?>
 
 <!DOCTYPE html>
@@ -12,9 +23,12 @@ session_start();
     <link rel="stylesheet" href="Deconnexion.css">
 </head>
 <body>
-
-<img src="/Model/CreationDeCompte/LPS1.0.png">
-
+<nav class="navbar">
+<div class="navbar-left">
+    <img src="/Model/Accueil/LPS1.0.png" alt="Logo" class="logo"/>
+    <span class="app-name">Le Petit Stage</span>
+</div>
+</nav>
 <!-- Carré d'information sur la déconnexion -->
 <article class="carré">
 
@@ -25,7 +39,7 @@ session_start();
     <h3>Vous avez été déconnecté avec succès.</h3>
 
 <p class="textCenter">
-    <a href="../CreationDeCompte/index.php">Retour à l'accueil</a> <br>
+    <a href="../Accueil/index.php">Retour à l'accueil</a> <br>
 </p>
 <p>
     Si vous ne souhaitez pas revenir à l'accueil, pour des raisons de sécurité, veuillez fermer cette page.
