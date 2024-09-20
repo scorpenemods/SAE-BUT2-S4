2de8570c725b44d29a22aa5ee857493f13cd279a
jubrg <julien.bourgeois4@icloud.com>
2024-09-20 11:06:24 +0200

    finitions css paramètres

--- Model/Parametre/Information/Information.php
+++ Model/Parametre/Information/Information.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informations du compte</title>
-    <link rel="stylesheet" href="Information.css">
+    <link rel="stylesheet" href="../Parametre/Information.css">
+    <script type="text/javascript" src="./Parametre/Paremetre.js"></script>
 </head>
 <body>
     <section class="compte-info">
--- Model/Parametre/Preference/Preference.php
+++ Model/Parametre/Preference/Preference.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Le Petit Stage - Préférences</title>
-    <link rel="stylesheet" href="Preference.css">
+    <link rel="stylesheet" href="../Parametre/Preference.css">
 </head>
 <body>
 <!-- Preferences section -->
