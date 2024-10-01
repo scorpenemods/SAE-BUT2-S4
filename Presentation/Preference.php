fefd38ca0bd5bdc6a7c4a4a92ce76de2bf3bd1ee
Marion <Marion.Hethuin@uphf.fr>
2024-10-01 11:55:32 +0200

    préférences visuel fait

--- Presentation/Preference.php
+++ Presentation/Preference.php
@@ -7,38 +7,50 @@
     <link rel="stylesheet" href="../View/Settings/Preference.css">
 </head>
 <body>
+<nav class="navbar">
+    <div class="navbar-left">
+        <img src="../Resources/LPS%201.0.png" alt="Logo" class="logo"/>
+        <span class="app-name">Le Petit Stage</span>
+    </div>
+</nav>
 <!-- Preferences section -->
 <main>
     <h2>Préférences</h2>
     <div class="preferences">
         <div class="preference-item">
             <span>Notification :</span>
+            <span>Off</span>
             <label class="switch">
                 <input type="checkbox" checked>
                 <span class="slider"></span>
             </label>
             <span>On</span>
-            <span>Off</span>
         </div>
         <div class="preference-item">
             <span>A2F :</span>
+            <span>Off</span>
             <label class="switch">
                 <input type="checkbox" checked>
                 <span class="slider"></span>
             </label>
             <span>On</span>
-            <span>Off</span>
         </div>
         <div class="preference-item">
             <span>Cookies :</span>
+            <span>Nécessaire</span>
             <label class="switch">
                 <input type="checkbox">
                 <span class="slider"></span>
             </label>
-            <span>Nécessaire</span>
             <span>Tous</span>
         </div>
     </div>
 </main>
+
+<footer>
+    <img src="../Resources/Logo_UPHF.png" alt="Logo uphf" width="10%">
+    <a href="Redirection.php">Informations</a>
+    <a href="Redirection.php">A propos</a>
+</footer>
 </body>
 </html>
