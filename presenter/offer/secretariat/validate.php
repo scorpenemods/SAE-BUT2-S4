da53e14e2f739e03d3c3ae2f4da89d28fdbc2a79
Lorenzo Warnier <lorenzo62.pilou@gmail.com>
2024-10-03 08:08:34 +0200

    Fix Path

--- presenter/offer/secretariat/validate.php
+++ presenter/offer/secretariat/validate.php
@@ -5,12 +5,15 @@ require dirname(__FILE__) . '/../../../models/Company.php';
 
 if (isset($_POST['id']) && isset($_SERVER["HTTP_REFERER"])) {
     $offer = PendingOffer::getById($_POST['id']);
-    if ($offer->getOfferId() == 0) {
-        $company_id = $offer->getCompanyId();
-        Offer::create($company_id, $offer->getTitle(), $offer->getDescription(), $offer->getJob(), $offer->getDuration(), $offer->getSalary(), $offer->getAddress(), $offer->getStudyLevel(), $offer->getBeginDate(), $offer->getTags(), $offer->getEmail(), $offer->getPhone(), '', '', 0);
-        PendingOffer::setStatus($offer->getId(), "Accepted");
-    } else {
-        Offer::update($offer->getOfferId(), $offer->getTitle(), $offer->getDescription(), $offer->getJob(), $offer->getDuration(), $offer->getSalary(), $offer->getAddress(), $offer->getStudyLevel(), $offer->getBeginDate(), $offer->getTags(), $offer->getEmail(), $offer->getPhone(), '', '', 0);
-        PendingOffer::setStatus($offer->getId(), "Accepted");
+    if ($offer->getStatus() == "Pending") {
+        if ($offer->getOfferId() == 0) {
+            $company_id = $offer->getCompanyId();
+            Offer::create($company_id, $offer->getTitle(), $offer->getDescription(), $offer->getJob(), $offer->getDuration(), $offer->getSalary(), $offer->getAddress(), $offer->getStudyLevel(), $offer->getBeginDate(), $offer->getTags(), $offer->getEmail(), $offer->getPhone(), '', '', 0);
+            PendingOffer::setStatus($offer->getId(), "Accepted");
+        } else {
+            Offer::update($offer->getOfferId(), $offer->getTitle(), $offer->getDescription(), $offer->getJob(), $offer->getDuration(), $offer->getSalary(), $offer->getAddress(), $offer->getStudyLevel(), $offer->getBeginDate(), $offer->getTags(), $offer->getEmail(), $offer->getPhone(), '', '', 0);
+            PendingOffer::setStatus($offer->getId(), "Accepted");
+        }
     }
+    header("Location: ../../../view/pending/list.php");
 }
