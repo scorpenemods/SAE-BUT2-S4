6ae928a5e649727f0ac684faeca887f160075b22
noahlemr <noahlemr@gmail.com>
2024-10-11 12:01:15 +0200

    noah

--- Presentation/EmailValidationNotice.php
+++ Presentation/EmailValidationNotice.php
@@ -5,7 +5,9 @@ require_once "../vendor/autoload.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 date_default_timezone_set('Europe/Paris');
 
 $database = new Database();
