971b3c70e0749ca7b7b22fcdbc2d6312bb9a34e6
LiRuZ <75847861+Liruz62@users.noreply.github.com>
2024-11-06 16:30:02 +0100

    Update database.php

--- presenter/database.php
+++ presenter/database.php
@@ -3,7 +3,7 @@ global $db;
 
 try {
     if (!isset($db)) {
-        $db = new pdo('mysql:host=82.66.53.143:64893;dbname=offers_tables','sae','sae2024', array(PDO::ATTR_PERSISTENT => true));
+        $db = new pdo('mysql:host=db.liruz.fr:3306;dbname=offers_tables','sae','sae2024', array(PDO::ATTR_PERSISTENT => true));
     }
 } catch(PDOException $ex) {
     echo $ex->getMessage();
