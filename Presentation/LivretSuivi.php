f5bd8421e574e5cca5003455190569f07c2aa07b
Marion <Marion.Hethuin@uphf.fr>
2024-11-08 17:00:34 +0100

    Livret de suivi

--- Presentation/LivretSuivi.php
+++ Presentation/LivretSuivi.php
@@ -4,6 +4,7 @@
 
 
 <body>
+<h3>Livret de suivi de</h3><br>
 <div class="livret-container">
     <aside class="livretbar">
         <span class="vignette" onclick="showContent(0)">1ère rencontre</span><br>
@@ -12,10 +13,17 @@
         <span class="vignette" onclick="showContent(3)">Dépôt du mémoire ou rapport</span><br>
     </aside>
 
-    <div class="content-container">
+    <div class="content-livret">
+        <h3>Stage suivi par </h3><br>
+
+        <div class="livret-header">
+            <h3>Entreprise </h3>
+        </div><br>
+
+
+
         <div class="content-section" id="0">
             <h2>1ère rencontre</h2>
-            <p>Content for the first meeting.</p>
         </div>
         <div class="content-section" id="1">
             <h2>2ème rencontre</h2>
