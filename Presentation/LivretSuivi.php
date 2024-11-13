c8cefa57cbe858c64f308022e815edae65c6f925
Marion <Marion.Hethuin@uphf.fr>
2024-11-13 11:20:39 +0100

    Livret de suivi base de l'affichage

--- Presentation/LivretSuivi.php
+++ Presentation/LivretSuivi.php
@@ -1,11 +1,25 @@
 <?php
-$etudiant='Lucie';
-$professeur='Julien';
-$MDS='Marie';
+$etudiant="Lucie <br> 
+Email : lucie@gmail.com <br>
+Telephone : 0657284298 <br>
+Description : fezrgzgfiqfhuHTY";
+
+$professeur="Julien <br> 
+Email : Julien@gmail.com <br>
+Telephone : 0657284298 <br>
+Description : fezrgzgfiqfhuHTY";
+
+$MDS="Marie <br> 
+Email : Marie@gmail.com <br>
+Telephone : 0657284298 <br>
+Description : fezrgzgfiqfhuHTY";
 ?>
 
 
 <body>
+
+<!-- Création des différentes rencontres / dépôts : -->
+
 <div class="livret-container">
     <aside class="livretbar">
         <span class="vignette" onclick="showContent(0)">1ère rencontre</span><br>
@@ -14,12 +28,15 @@ $MDS='Marie';
         <span class="vignette" onclick="showContent(3)">Dépôt du mémoire ou rapport</span><br>
     </aside>
 
+
+    <!-- Affichage des informations des participants : -->
+
     <div class="content-livret">
 
         <div class="livret-header">
-            <h3>Participants </h3>
+            <h2>Participants </h2>
         </div>
-        <div style="display: flex; gap: 20px;">
+        <div style="display: flex; gap: 200px;">
             <div class="participants">
                 <h3>Etudiant :</h3>
                 <p><?php echo $etudiant; ?></p>
@@ -37,23 +54,41 @@ $MDS='Marie';
         </div>
 
 
-
-
+        <!-- Les différents formulaire pour chaque rencontre : -->
 
         <div class="content-section" id="0">
-            <h2>1ère rencontre</h2>
+            <h3 style="padding: 10px">Formulaire</h3>
+            <div class="livret-header">
+                <h3>1ère rencontre</h3>
+            </div>
+
+            <p class="participants">Content for the first meeting.</p>
         </div>
+
         <div class="content-section" id="1">
-            <h2>2ème rencontre</h2>
-            <p>Content for the second meeting.</p>
+            <h3 style="padding: 10px">Formulaire</h3>
+            <div class="livret-header">
+                <h3>2ème rencontre</h3>
+            </div>
+
+            <p class="participants">Content for the second meeting.</p>
         </div>
+
         <div class="content-section" id="2">
-            <h2>3ème rencontre</h2>
-            <p>Content for the third meeting.</p>
+            <h3 style="padding: 10px">Formulaire</h3>
+            <div class="livret-header">
+                <h3>3ème rencontre</h3>
+            </div>
+
+            <p class="participants">Content for the third meeting.</p>
         </div>
+
         <div class="content-section" id="3">
-            <h2>Dépôt du mémoire ou rapport</h2>
-            <p>Content for the report submission.</p>
+            <div class="livret-header">
+                <h3>Dépôt du mémoire ou rapport</h3>
+            </div>
+
+            <p class="participants">Content for the report submission.</p>
         </div>
     </div>
 
