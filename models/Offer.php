b5c4ec54f8e6dc32a41b39304a2da1a75140ceb9
UnderscoreThibaut <contact@underscorethibaut.fr>
2024-11-18 09:09:07 +0100

    cleaned + commented Offer.php

--- models/Offer.php
+++ models/Offer.php
@@ -2,7 +2,10 @@
 
 require dirname(__FILE__) . '/../presenter/database.php';
 
-//Class to manage offers
+/**
+ * Offer
+ * Represents a Offer in the database
+ */
 class Offer {
     private int $id;
     private int $company_id;
@@ -22,7 +25,28 @@ class Offer {
     private string $created_at;
     private string $updated_at;
 
-    public function __construct(int $id, int $company_id, Company $company, string $title, string $description, string $job, int $duration, string $begin_date, int $salary, string $address, string $study_level, bool $is_active, string $email, string $phone, string $website, string $created_at, string $updated_at) {
+    /**
+     * __construct
+     * Constructor used to instantiate the object, used only internally
+     * @param int $id
+     * @param int $company_id
+     * @param Company $company
+     * @param string $title
+     * @param string $description
+     * @param string $job
+     * @param int $duration
+     * @param string $begin_date
+     * @param int $salary
+     * @param string $address
+     * @param string $study_level
+     * @param bool $is_active
+     * @param string $email
+     * @param string $phone
+     * @param string $website
+     * @param string $created_at
+     * @param string $updated_at
+     */
+    private function __construct(int $id, int $company_id, Company $company, string $title, string $description, string $job, int $duration, string $begin_date, int $salary, string $address, string $study_level, bool $is_active, string $email, string $phone, string $website, string $created_at, string $updated_at) {
         $this->id = $id;
         $this->company_id = $company_id;
         $this->company = $company;
@@ -42,74 +66,198 @@ class Offer {
         $this->updated_at = $updated_at;
     }
 
+    /**
+     * instantiateRows
+     * Utility function used to instantiate the rows of the result set
+     * @param false|PDOStatement $stmt
+     * @return array
+     */
+    private static function instantiateRows(false|PDOStatement $stmt): array {
+        $result = $stmt->fetchAll();
 
+        $offers = [];
+        foreach ($result as $row) {
+            $company = Company::getById($row["company_id"]);
+
+            if (!$company) {
+                continue;
+            }
+
+            $offers[] = new Offer(
+                $row["id"],
+                $row["company_id"],
+                $company,
+                $row["title"],
+                $row["description"],
+                $row["job"],
+                $row["duration"],
+                $row["begin_date"],
+                $row["salary"],
+                $row["address"],
+                $row["study_level"],
+                $row["is_active"],
+                $row["email"],
+                $row["phone"],
+                $row["website"],
+                date("Y-m-d H:i:s", strtotime($row["created_at"])),
+                date("Y-m-d H:i:s", strtotime($row["updated_at"]))
+            );
+        }
+
+        return $offers;
+    }
+
+
+    /**
+     * getId
+     * Returns the id of the offer
+     * @return int
+     */
     public function getId(): int {
         return $this->id;
     }
 
+    /**
+     * getCompanyId
+     * Returns the id of the company
+     * @return int
+     */
     public function getCompanyId(): int {
         return $this->company_id;
     }
 
+    /**
+     * getCompany
+     * Returns the company
+     * @return Company
+     */
     public function getCompany(): Company {
         return $this->company;
     }
 
+    /**
+     * getTitle
+     * Returns the title of the offer
+     * @return string
+     */
     public function getTitle(): string {
         return $this->title;
     }
 
+    /**
+     * getDescription
+     * Returns the description of the offer
+     * @return string
+     */
     public function getDescription(): string {
         return $this->description;
     }
 
+    /**
+     * getJob
+     * Returns the job title of the offer
+     * @return string
+     */
     public function getJob(): string {
         return $this->job;
     }
 
+    /**
+     * getDuration
+     * Returns the duration of the offer
+     * @return int
+     */
     public function getDuration(): int {
         return $this->duration;
     }
 
+    /**
+     * getBeginDate
+     * Returns the begin date of the offer
+     * @return string
+     */
     public function getBeginDate(): string {
         return $this->begin_date;
     }
 
+    /**
+     * getSalary
+     * Returns the salary of the offer
+     * @return int
+     */
     public function getSalary(): int {
         return $this->salary;
     }
 
+    /**
+     * getAddress
+     * Returns the address of the offer
+     * @return string
+     */
     public function getAddress(): string {
         return $this->address;
     }
 
+    /**
+     * getStudyLevel
+     * Returns the study level of the offer
+     * @return string
+     */
     public function getStudyLevel(): string {
         return $this->study_level;
     }
 
+    /**
+     * getIsActive
+     * Returns the is active flag of the offer
+     * @return bool
+     */
     public function getIsActive(): bool {
         return $this->is_active;
     }
 
+    /**
+     * getEmail
+     * Returns the email of the offer
+     * @return string
+     */
     public function getEmail(): string {
         return $this->email;
     }
 
+    /**
+     * getPhone
+     * Returns the phone of the offer
+     * @return string
+     */
     public function getPhone(): string {
         return $this->phone;
     }
 
+    /**
+     * getCreatedAt
+     * Returns the created at date of the offer
+     * @return string
+     */
     public function getCreatedAt(): string {
         return $this->created_at;
     }
 
-    public function getUpdatedAt(): string
-    {
+    /**
+     * getUpdatedAt
+     * Returns the updated at date of the offer
+     * @return string
+     */
+    public function getUpdatedAt(): string {
         return $this->updated_at;
     }
 
-    public function getTags(): ?array
-    {
+    /**
+     * getTags
+     * Returns the tags of the offer
+     * @return array|null
+     */
+    public function getTags(): ?array {
         global $db;
 
         $stmt = $db->prepare("SELECT * FROM tags JOIN tags_offers ON tags.id = tags_offers.tag_id WHERE tags_offers.offer_id = :offer_id");
@@ -130,11 +278,27 @@ class Offer {
         return $tags;
     }
 
-    //Update an offer
+    /**
+     * update
+     * Updates the offer with the given id with the given data in the database
+     * @param int $getId
+     * @param string $getTitle
+     * @param string $getDescription
+     * @param string $getJob
+     * @param int $getDuration
+     * @param int $getSalary
+     * @param string $getAddress
+     * @param string $getEducation
+     * @param string $getBeginDate
+     * @param array|null $getTags
+     * @param string $getEmail
+     * @param string $getPhone
+     * @param string $getWebsite
+     * @return Offer|null
+     */
     public static function update(int $getId, string $getTitle, string $getDescription, string $getJob, int $getDuration, int $getSalary, string $getAddress, string $getEducation, string $getBeginDate, ?array $getTags, string $getEmail, string $getPhone, string $getWebsite) {
         global $db;
 
-        //Update the offer
         $stmt = $db->prepare("UPDATE offers SET title = :title, description = :description, job = :job, duration = :duration, salary = :salary, address = :address, study_level = :study_level, begin_date = :begin_date, email = :email, phone = :phone, website = :website WHERE id = :id");
         $stmt->bindParam(":title", $getTitle);
         $stmt->bindParam(":description", $getDescription);
@@ -155,7 +319,6 @@ class Offer {
             return null;
         }
 
-        //Delete the tags in the tags_offers table
         $stmt = $db->prepare("DELETE FROM tags_offers WHERE offer_id = :id");
         $stmt->bindParam(":id", $getId);
         $stmt->execute();
@@ -164,7 +327,6 @@ class Offer {
             return null;
         }
 
-        //Add the tags in the tags_offers table
         foreach ($getTags as $tag) {
             $stmt = $db->prepare("INSERT INTO tags_offers (tag_id, offer_id) VALUES ((SELECT tag FROM tags WHERE id = :tag_id), :offer_id)");
             $stmt->bindParam(":tag_id", $tag);
@@ -176,6 +338,11 @@ class Offer {
         return $offer;
     }
 
+    /**
+     * getDomain
+     * Returns the domain of the website
+     * @return string|null
+     */
     public function getDomain(): ?string {
         $fullDomain = parse_url($this->website, PHP_URL_HOST);
 
@@ -184,30 +351,23 @@ class Offer {
         return $matches[1] ?? null;
     }
 
+    /**
+     * getImage
+     * Returns the image URL of the website
+     * @return string|null
+     */
     public function getImage(): ?string {
         $imagePath = 'https://cdn.brandfetch.io/' . $this->getDomain() . '/w/512/h/512';
         return $imagePath;
     }
 
-    public function getBackgroundColor() {
-        $imagePath = $this->getImage();
-        $image = imagecreatefromwebp($imagePath);
-
-        $width = imagesx($image);
-        $height = imagesy($image);
-
-        $rgb = imagecolorat($image, 0, 0);
-
-        $r = ($rgb >> 16) & 0xFF;
-        $g = ($rgb >> 8) & 0xFF;
-        $b = $rgb & 0xFF;
-
-        imagedestroy($image);
-    }
-
-    //Get an offer by its id
-    public static function getById(int $id): ?Offer
-    {
+    /**
+     * getById
+     * Returns the offer with the given id
+     * @param int $id
+     * @return Offer|null
+     */
+    public static function getById(int $id): ?Offer {
         global $db;
 
         $stmt = $db->prepare("SELECT * FROM offers WHERE id = :id");
@@ -246,9 +406,12 @@ class Offer {
         );
     }
 
-    //Get all offers
-    public static function getAll(): ?array
-    {
+    /**
+     * getAll
+     * Returns all the offers
+     * @return array|null
+     */
+    public static function getAll(): ?array {
         global $db;
 
         $stmt = $db->prepare("SELECT * FROM offers");
@@ -258,45 +421,30 @@ class Offer {
             return null;
         }
 
-        $result = $stmt->fetchAll();
-
-        $offers = [];
-        foreach ($result as $row) {
-            $company = Company::getById($row["company_id"]);
-
-            if (!$company) {
-                continue;
-            }
-
-            $offers[] = new Offer(
-                $row["id"],
-                $row["company_id"],
-                $company,
-                $row["title"],
-                $row["description"],
-                $row["job"],
-                $row["duration"],
-                $row["begin_date"],
-                $row["salary"],
-                $row["address"],
-                $row["study_level"],
-                $row["is_active"],
-                $row["email"],
-                $row["phone"],
-                $row["website"],
-                date("Y-m-d H:i:s", strtotime($row["created_at"])),
-                date("Y-m-d H:i:s", strtotime($row["updated_at"]))
-            );
-        }
-
-        return $offers;
+        return self::instantiateRows($stmt);
     }
 
-    //Create a new offer
+    /**
+     * create
+     * Creates a new offer, inserts it into the database and returns the id
+     * @param int $company_id
+     * @param string $title
+     * @param string $description
+     * @param string $job
+     * @param int $duration
+     * @param int $salary
+     * @param string $address
+     * @param string $education
+     * @param string $begin_date
+     * @param array $tags
+     * @param string $email
+     * @param string $phone
+     * @param string $website
+     * @return Offer|null
+     */
     public static function create(int $company_id, string $title, string $description, string $job, int $duration, int $salary, string $address, string $education, string $begin_date, array $tags, string $email, string $phone, string $website) {
         global $db;
 
-        //Insert the offer in the offers table
         $stmt = $db->prepare("INSERT INTO offers (company_id, title, description, job , duration, salary, address,  study_level, begin_date,
                     email, phone, website) VALUES (:company_id, :title, :description, :job, :duration, :salary, :address, :study_level, :begin_date,
                     :email, :phone, :website)");
@@ -321,7 +469,6 @@ class Offer {
 
         $id = $db->lastInsertId();
 
-        //Add the tags in the tags_offers table
         foreach ($tags as $tag) {
             $stmt = $db->prepare("INSERT INTO tags_offers (tag_id, offer_id) VALUES ((SELECT tag FROM tags WHERE id = :tag_id), :offer_id)");
             $stmt->bindParam(":tag_id", $tag);
@@ -352,9 +499,12 @@ class Offer {
         return $offer;
     }
 
-    //Get the real duration of the offer using modulo
-    public function getRealDuration(): string
-    {
+    /**
+     * getRealDuration
+     * Get a human-readable duration of the offer using modulo to get the years, months and days
+     * @return string
+     */
+    public function getRealDuration(): string {
         $duration = $this->getDuration();
 
         $years = intdiv($duration, 365);
@@ -384,9 +534,12 @@ class Offer {
         return rtrim($result, ', ');
     }
 
-    //Get all tags
-    public static function getAllTags(): array
-    {
+    /**
+     * getAllTags
+     * Returns all the tags
+     * @return array
+     */
+    public static function getAllTags(): array {
         global $db;
 
         $stmt = $db->prepare("SELECT * FROM tags;");
@@ -402,13 +555,20 @@ class Offer {
         return $tags;
     }
 
+    /**
+     * @return string
+     */
     public function getWebsite() {
         return $this->website;
     }
 
-    //Get all offers of a company
-    public static function getCompanyOffers($companyId): ?array
-    {
+    /**
+     * getCompanyOffers
+     * Returns all the offers of the given company
+     * @param $companyId
+     * @return array|null
+     */
+    public static function getCompanyOffers($companyId): ?array {
         global $db;
 
         $stmt = $db->prepare("SELECT * FROM offers WHERE company_id = :company_id;");
@@ -419,41 +579,16 @@ class Offer {
             return null;
         }
 
-        $result = $stmt->fetchAll();
-
-        $offers = [];
-        foreach ($result as $row) {
-            $company = Company::getById($row["company_id"]);
-
-            if (!$company) {
-                continue;
-            }
-
-            $offers[] = new Offer(
-                $row["id"],
-                $row["company_id"],
-                $company,
-                $row["title"],
-                $row["description"],
-                $row["job"],
-                $row["duration"],
-                $row["begin_date"],
-                $row["salary"],
-                $row["address"],
-                $row["study_level"],
-                $row["is_active"],
-                $row["email"],
-                $row["phone"],
-                $row["website"],
-                date("Y-m-d H:i:s", strtotime($row["created_at"])),
-                date("Y-m-d H:i:s", strtotime($row["updated_at"]))
-            );
-        }
-
-        return $offers;
+        return self::instantiateRows($stmt);
     }
 
-    //Get all offers filtered by the filters
+    /**
+     * getFilteredOffers
+     * Returns the filtered + paginated offers
+     * @param int $n
+     * @param array $filters
+     * @return array|null
+     */
     public static function getFilteredOffers(int $n, array $filters): ?array {
         global $db;
 
@@ -601,7 +736,12 @@ class Offer {
         return [$offers, ceil($count / 12)];
     }
 
-    //Hide an offer
+    /**
+     * hide
+     * Hides the offer with the given id
+     * @param $id
+     * @return true|null
+     */
     public static function hide($id) {
         global $db;
 
@@ -616,6 +756,13 @@ class Offer {
         return true;
     }
 
+    /**
+     * isCompanyOffer
+     * Returns true if the offer with the given id is from the given company
+     * @param int $id
+     * @param int $company_id
+     * @return bool|null
+     */
     public static function isCompanyOffer(int $id, int $company_id): ?bool {
         global $db;
 
@@ -637,7 +784,12 @@ class Offer {
         return true;
     }
 
-    // Verify if an offer is already pending
+    /**
+     * isAlreadyPending
+     * Returns true if the offer with the given id is already pending
+     * @param int $id
+     * @return bool|null
+     */
     public static function isAlreadyPending(int $id): ?bool {
         global $db;
 
@@ -658,7 +810,13 @@ class Offer {
         return true;
     }
 
-    // Add a favorite offer for a user
+    /**
+     * makeFavorite
+     * Adds the offer with the given id to the user's favorite offers
+     * @param int $id
+     * @param int $user_id
+     * @return bool|null
+     */
     public static function makeFavorite(int $id, int $user_id): ?bool {
         global $db;
 
@@ -674,7 +832,13 @@ class Offer {
         return true;
     }
 
-    // Remove a favorite offer for a user
+    /**
+     * removeFavorite
+     * Removes the offer with the given id from the user's favorite offers
+     * @param int $id
+     * @param int $user_id
+     * @return bool|null
+     */
     public static function removeFavorite(int $id, int $user_id): ?bool {
         global $db;
 
@@ -690,7 +854,13 @@ class Offer {
         return true;
     }
 
-    // Verify if a user has a favorite offer
+    /**
+     * isFavorite
+     * Returns true if the offer with the given id is in the user's favorite offers
+     * @param int $id
+     * @param int $user_id
+     * @return bool|null
+     */
     public static function isFavorite(int $id, int $user_id): ?bool {
         global $db;
 
@@ -712,7 +882,12 @@ class Offer {
         return true;
     }
 
-    // Get all inactive offers
+    /**
+     * getAllInactive
+     * Returns all the inactive offers
+     * @param int $company_id
+     * @return array|null
+     */
     public static function getAllInactive(int $company_id = 0): ?array {
         global $db;
         if ($company_id != 0) {
@@ -727,37 +902,6 @@ class Offer {
             return null;
         }
 
-        $result = $stmt->fetchAll();
-
-        $offers = [];
-        foreach ($result as $row) {
-            $company = Company::getById($row["company_id"]);
-
-            if (!$company) {
-                continue;
-            }
-
-            $offers[] = new Offer(
-                $row["id"],
-                $row["company_id"],
-                $company,
-                $row["title"],
-                $row["description"],
-                $row["job"],
-                $row["duration"],
-                $row["begin_date"],
-                $row["salary"],
-                $row["address"],
-                $row["study_level"],
-                $row["is_active"],
-                $row["email"],
-                $row["phone"],
-                $row["website"],
-                date("Y-m-d H:i:s", strtotime($row["created_at"])),
-                date("Y-m-d H:i:s", strtotime($row["updated_at"]))
-            );
-            }
-
-        return $offers;
+        return self::instantiateRows($stmt);
     }
 }
