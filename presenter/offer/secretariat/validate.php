0b5d5fe8660f85a413f89b709c1a7a95cff4a6d4
Margot <margot.terrier@uphf.fr>
2024-11-20 09:26:40 +0100

    fonctionnalité demande de notification

--- presenter/offer/secretariat/validate.php
+++ presenter/offer/secretariat/validate.php
@@ -4,6 +4,8 @@ session_start();
 require dirname(__FILE__) . '/../../../models/PendingOffer.php';
 require dirname(__FILE__) . '/../../../models/Company.php';
 require dirname(__FILE__) . '/../../../models/Database.php';
+require dirname(__FILE__) . '/../../../presenter/offer/notify.php';
+
 
 
 
@@ -12,7 +14,8 @@ if (isset($_SESSION['secretariat']) && isset($_POST['id']) && isset($_SERVER["HT
     if ($offer->getStatus() == "Pending") {
         if ($offer->getOfferId() == 0) {
             $company_id = $offer->getCompanyId();
-            Offer::create($company_id, $offer->getTitle(), $offer->getDescription(), $offer->getJob(), $offer->getDuration(), $offer->getSalary(), $offer->getAddress(), $offer->getStudyLevel(), $offer->getBeginDate(), $offer->getTags(), $offer->getEmail(), $offer->getPhone(), $offer->getWebsite());
+            $offer = Offer::create($company_id, $offer->getTitle(), $offer->getDescription(), $offer->getJob(), $offer->getDuration(), $offer->getSalary(), $offer->getAddress(), $offer->getStudyLevel(), $offer->getBeginDate(), $offer->getTags(), $offer->getEmail(), $offer->getPhone(), $offer->getWebsite());
+            sendNotification($offer);
         } else {
             Offer::update($offer->getOfferId(), $offer->getTitle(), $offer->getDescription(), $offer->getJob(), $offer->getDuration(), $offer->getSalary(), $offer->getAddress(), $offer->getStudyLevel(), $offer->getBeginDate(), $offer->getTags(), $offer->getEmail(), $offer->getPhone(), $offer->getWebsite());
         }
