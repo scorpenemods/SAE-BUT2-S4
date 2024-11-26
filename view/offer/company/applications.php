3251d5482704c78bd1700ed136da985eb8d2c23e
UnderscoreThibaut <contact@underscorethibaut.fr>
2024-11-26 15:07:32 +0100

    cosmetic refactor

--- view/offer/company/applications.php
+++ view/offer/company/applications.php
@@ -39,89 +39,91 @@ if ($groupeSecretariat || ($company_id != 0 && Offer::isCompanyOffer($offerId, $
     <link rel="stylesheet" href="../../css/header.css">
     <link rel="stylesheet" href="../../css/applications.css">
     <link rel="stylesheet" href="../../css/footer.css">
+    <link rel="stylesheet" href="../../css/notification.css">
+    <script src="../../js/notification.js"></script>
 </head>
 <body>
-<?php include '../../header.php' ?>
-<main>
-    <div class="spacer">
-        <div class="card">
-            <h2>Liste des Candidatures pour <?php echo Applications::getOfferName($offerId);?></h2>
-            <div id="liste-candidats"></div>
-            <?php
-                if ($applications) {
-                    foreach($applications as $apply){
-                        $id_user = $apply->getIdUser();
-                        echo "<div class='candidat'>";
-                        echo "<div class='info'>";
-                        echo "<h3 class='nom'>".Applications::getUsername($id_user)."</h3>";
-                        echo "<ul class='fichiers'>";
-                        //Todo
-                        echo "<li class='fichier' onclick='getFile(\"" . $id_user . "\", \"" . $offerId . "\", \"cv\")'>📎 CV</li>";
-                        echo "<li class='fichier' onclick='getFile(\"" . $id_user . "\", \"" . $offerId . "\", \"motivation\")'>📎 Lettre de motivation</li>";
-                        echo "</ul>";
-                        echo "</div>";
-                        echo "<span class='date'>".$apply->getCreatedAt()."</span>";
-                        if (!$groupeSecretariat) {
-                            echo "<div class='actions'>";
-                                echo "<form action='../../../presenter/offer/applications/validate.php' method='post'>";
-                                    echo "<input type='hidden' name='id_offer' value='" . $offerId . "'>";
-                                    echo "<input class='button accept' type='submit' name='Valider' value='Valider'>";
-                                echo "</form>";
-                                echo "<form action='../../../presenter/offer/applications/validate.php' method='post'>";
-                                    echo "<input type='hidden' name='id_offer' value='" . $offerId . "'>";
-                                    echo "<input class='button refuse' id='refuseButton' type='submit' name='Refuser' value='Refuser'>";
-                                echo "</form>";
+    <?php include '../../header.php' ?>
+    <main>
+        <div class="spacer">
+            <div class="card">
+                <h2>Liste des Candidatures pour <?php echo Applications::getOfferName($offerId);?></h2>
+                <div id="liste-candidats"></div>
+                <?php
+                    if ($applications) {
+                        foreach($applications as $apply){
+                            $id_user = $apply->getIdUser();
+                            echo "<div class='candidat'>";
+                            echo "<div class='info'>";
+                            echo "<h3 class='nom'>".Applications::getUsername($id_user)."</h3>";
+                            echo "<ul class='fichiers'>";
+                            //Todo
+                            echo "<li class='fichier' onclick='getFile(\"" . $id_user . "\", \"" . $offerId . "\", \"cv\")'>📎 CV</li>";
+                            echo "<li class='fichier' onclick='getFile(\"" . $id_user . "\", \"" . $offerId . "\", \"motivation\")'>📎 Lettre de motivation</li>";
+                            echo "</ul>";
+                            echo "</div>";
+                            echo "<span class='date'>".$apply->getCreatedAt()."</span>";
+                            if (!$groupeSecretariat) {
+                                echo "<div class='actions'>";
+                                    echo "<form action='../../../presenter/offer/applications/validate.php' method='post'>";
+                                        echo "<input type='hidden' name='id_offer' value='" . $offerId . "'>";
+                                        echo "<input class='button accept' type='submit' name='Valider' value='Valider'>";
+                                    echo "</form>";
+                                    echo "<form action='../../../presenter/offer/applications/validate.php' method='post'>";
+                                        echo "<input type='hidden' name='id_offer' value='" . $offerId . "'>";
+                                        echo "<input class='button refuse' id='refuseButton' type='submit' name='Refuser' value='Refuser'>";
+                                    echo "</form>";
+                                echo "</div>";
+                            }
                             echo "</div>";
                         }
-                        echo "</div>";
+                    } else {
+                        echo "No applications found";
                     }
-                } else {
-                    echo "No applications found";
-                }
-            ?>
+                ?>
+            </div>
         </div>
-    </div>
-</main>
-<?php include '../../footer.php'; ?>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    function getFile(id_user, id_offer, type_str) {
-        $.ajax({
-            url: '../../../presenter/offer/getfile.php',
-            method: 'POST',
-            data: { user: id_user, offer: id_offer, type: type_str },
-            xhrFields: {
-                responseType: 'blob'
-            },
-            success: function(data, status, xhr) {
-                let disposition = xhr.getResponseHeader('Content-Disposition');
-                let matches = /"([^"]*)"/.exec(disposition);
-                let filename = (matches != null && matches[1] ? matches[1] : fileName + '.pdf');
+    </main>
+    <?php include '../../footer.php'; ?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        function getFile(id_user, id_offer, type_str) {
+            $.ajax({
+                url: '../../../presenter/offer/getfile.php',
+                method: 'POST',
+                data: { user: id_user, offer: id_offer, type: type_str },
+                xhrFields: {
+                    responseType: 'blob'
+                },
+                success: function(data, status, xhr) {
+                    let disposition = xhr.getResponseHeader('Content-Disposition');
+                    let matches = /"([^"]*)"/.exec(disposition);
+                    let filename = (matches != null && matches[1] ? matches[1] : fileName + '.pdf');
 
-                // Create blob link to download
-                let blob = new Blob([data], { type: 'application/pdf' });
-                let link = document.createElement('a');
-                link.href = window.URL.createObjectURL(blob);
-                link.download = filename;
+                    // Create blob link to download
+                    let blob = new Blob([data], { type: 'application/pdf' });
+                    let link = document.createElement('a');
+                    link.href = window.URL.createObjectURL(blob);
+                    link.download = filename;
 
-                // Append to html link element page
-                document.body.appendChild(link);
+                    // Append to html link element page
+                    document.body.appendChild(link);
 
-                // Start download
-                link.click();
+                    // Start download
+                    link.click();
 
-                // Clean up and remove the link
-                link.parentNode.removeChild(link);
-            },
-            error: function(xhr, _status, _error) {
-                if (xhr.status === 404) {
-                    alert('File not found.');
-                } else {
-                    alert('An error occurred while downloading the file.');
+                    // Clean up and remove the link
+                    link.parentNode.removeChild(link);
+                },
+                error: function(xhr, _status, _error) {
+                    if (xhr.status === 404) {
+                        sendNotification("Erreur", "Erreur", "Le fichier demandé n'existe pas");
+                    } else {
+                        sendNotification("Erreur", "Erreur", "Une erreur est survenue lors du téléchargement du fichier");
+                    }
                 }
+            });
             }
-        });
-        }
-</script>
-</body>
+    </script>
+    </body>
 </html>
