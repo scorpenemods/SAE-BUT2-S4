0312ce01a839f293109cfb4a53b9245cf7416acf
LiRuZ <lorenzo62.pilou@gmail.com>
2024-12-11 10:21:36 +0100

    Alter table sql

--- liquidbase/changelog/v4.sql
+++ liquidbase/changelog/v4.sql
@@ -1,2 +1,7 @@
 --liquibase formatted sql
 
+--changeset LiRuZ:23 labels:Modify-table
+--comment: Add title to alerts
+ALTER TABLE alerts
+ADD COLUMN title VARCHAR(255);
+--rollback ALTER TABLE alerts DROP COLUMN title;
