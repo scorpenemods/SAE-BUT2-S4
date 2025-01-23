62b95204881d9ff1d97d4745f570937071a9192f
noahlemr <noahlemr@gmail.com>
2025-01-23 08:50:10 +0100

    traduction

--- Presentation/MissionStage.php
+++ Presentation/MissionStage.php
@@ -3,7 +3,22 @@
 require_once "../Model/Database.php";
 // Instanciation de l'objet Database (singleton pour une seule instance de connexion)
 $database = (Database::getInstance());
+// Vérifier si une langue est définie dans l'URL, sinon utiliser la session ou le français par défaut
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+    $_SESSION['lang'] = $lang; // Enregistrer la langue en session
+} else {
+    $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'fr'; // Langue par défaut
+}
 
+// Vérification si le fichier de langue existe, sinon charger le français par défaut
+$langFile = "../Locales/{$lang}.php";
+if (!file_exists($langFile)) {
+    $langFile = "../Locales/fr.php";
+}
+
+// Charger les traductions
+$translations = include $langFile;
 
 function getFieldValue($field, $inputs = null, $default = 'Pas défini'): string
 {
