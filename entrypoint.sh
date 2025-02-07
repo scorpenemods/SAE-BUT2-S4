1c29092446b563869bec356a58d0112c26b0e7bc
Valerchk <valerlogv.32@gmail.com>
2025-02-07 12:08:43 +0100

    Deploy. Final debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+# Подставляем значение переменной PORT в шаблон nginx-конфигурации
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
 # Если директория vendor отсутствует или пуста, запускаем composer install
 if [ ! -d "/var/www/html/vendor" ] || [ -z "$(ls -A /var/www/html/vendor)" ]; then
     composer install --no-dev --optimize-autoloader
 fi
 
-# Запуск supervisor (и, соответственно, nginx и php-fpm)
+# Запуск supervisor (который стартует php-fpm и nginx)
 exec /usr/bin/supervisord -n
